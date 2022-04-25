de52fc9b1da47e4f2e53d6c16c804a81584cfe36
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2022-04-24 20:46:29 -0400

    Updated the 'Show more sweets' button to force fetching

--- components/Tweets/Tweets.jsx
+++ components/Tweets/Tweets.jsx
@@ -1,14 +1,21 @@
-import Link from 'next/link'
+import { useSWRConfig } from 'swr'
 import Tweet from './Tweet'
 import TweetForm from './TweetForm'
 
 const Tweets = ({ tweets = [] }) => {
+  const { mutate } = useSWRConfig()
+
   return (
     <>
       <TweetForm />
-      <Link href='/'>
-        <a className='block border-y p-3 text-center font-medium text-blue-500'>Show More Sweets</a>
-      </Link>
+      <div>
+        <button
+          onClick={() => mutate('/api/tweets')}
+          className='block border-y p-3 text-center font-medium text-blue-500'
+        >
+          Show More Sweets
+        </button>
+      </div>
       <ul>
         {tweets.map(tweet => (
           <Tweet key={tweet._id} tweet={tweet} />
